ce2b507e2fa43e8e40500048d87057aa22f54d9d
wov <wovfeng@gmail.com>
2015-04-22 13:57:02 +0800

    add detail page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Leaf' });
 });
 
+
+// get project page
+router.get('/:project/:version?', function(req, res, next) {
+    var version = req.params.version;
+    if(version === 'prd' || version === 'prototype' || version === 'visual'){
+        next();
+    }
+    res.render('index', { title: 'project' });
+});
+
+
+// get prd page
+router.get('/:project/:version?/prd', function(req, res, next) {
+  res.render('index', { title: 'prd' });
+});
+
+
+// get prototype page
+router.get('/:project/:version?/prototype', function(req, res, next) {
+  res.render('index', { title: 'prototype' });
+});
+
+
+// get visual page
+router.get('/:project/:version?/visual', function(req, res, next) {
+  res.render('index', { title: 'visual' });
+});
+
 module.exports = router;
