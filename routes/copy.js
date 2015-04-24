d128bca29526f56b2edc6c128e63cc9850ee8625
历风 <lb1064@qq.com>
2015-04-24 09:35:34 +0800

    捕获异常

--- routes/copy.js
+++ routes/copy.js
@@ -19,83 +19,79 @@ router.get('/', function(req, res, next) {
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-    var projectName = req.body.projectName;
-    var version = req.body.version;
-    var type = req.body.type;
-    var srcPath = req.body.srcPath;
+    try {
+        var projectName = req.body.projectName;
+        var version = req.body.version;
+        var type = req.body.type;
+        var srcPath = req.body.srcPath;
 
 
-    function findPath(srcPath) {
-        var realPath = srcPath;
-        if (srcPath.split('CrossShare/').length > 1) {
-            realPath = srcPath.split('CrossShare/')[1];
+        function findPath(srcPath) {
+            var realPath = srcPath;
+            if (srcPath.split('CrossShare/').length > 1) {
+                realPath = srcPath.split('CrossShare/')[1];
+            }
+            return realPath;
         }
-        return realPath;
-    }
 
 
 
-    srcPath = findPath(srcPath);
+        srcPath = findPath(srcPath);
 
-    // console.log('源目录=', srcPath);
-
-
-
-    //TODO
-    srcPath = path.join(srcRootPath, srcPath)
+        // console.log('源目录=', srcPath);
 
 
-    if (!fs.existsSync(srcPath)) {
-        res.send('error');
-        return;
-    }
+
+        //TODO
+        srcPath = path.join(srcRootPath, srcPath)
 
 
-    var file = fs.statSync(srcPath);
-
-    if (file.isDirectory()) {
-        srcPath = path.join(srcPath, path.sep);
-    }
-
-    // console.log('处理后=', srcPath)
-    //copy
-    var projectPath = path.join(projectRootPath, projectName, version);
-
-    var isExists = fs.existsSync(projectPath);
-
-    if (!isExists) {
-        fs.mkdirSync(projectPath);
-        fs.mkdirSync(path.join(projectPath, path.sep, 'prd'));
-        fs.mkdirSync(path.join(projectPath, path.sep, 'prototype'));
-        fs.mkdirSync(path.join(projectPath, path.sep, 'visual'));
-    }
-
-
-    var cmdStr = 'cp -R ' + srcPath + ' ' + projectPath + path.sep + type + path.sep;
-
-    // console.log('命令=', cmdStr);
-
-    exec(cmdStr, function(err, stdout, stderr) {
-        if (err) {
-            console.error(stderr);
+        if (!fs.existsSync(srcPath)) {
             res.send('error');
-        } else {
-            res.send('同步成功！');
+            return;
         }
-    });
+
+
+        var file = fs.statSync(srcPath);
+
+        if (file.isDirectory()) {
+            srcPath = path.join(srcPath, path.sep);
+        }
+
+        // console.log('处理后=', srcPath)
+        //copy
+        var projectPath = path.join(projectRootPath, projectName, version);
+
+        var isExists = fs.existsSync(projectPath);
+
+        if (!isExists) {
+            fs.mkdirSync(projectPath);
+            fs.mkdirSync(path.join(projectPath, path.sep, 'prd'));
+            fs.mkdirSync(path.join(projectPath, path.sep, 'prototype'));
+            fs.mkdirSync(path.join(projectPath, path.sep, 'visual'));
+        }
+
+
+        var cmdStr = 'cp -R ' + srcPath + ' ' + projectPath + path.sep + type + path.sep;
+
+        // console.log('命令=', cmdStr);
+
+        exec(cmdStr, function(err, stdout, stderr) {
+            if (err) {
+                console.error(stderr);
+                res.send('error');
+            } else {
+                res.send('同步成功！');
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+        res.send('error');
+    }
 
 
 
 });
-
-
-
-
-
-
-
-
-//copy('首页精准化', 1, 'visual', path.join(srcRootPath, '首页精准化v1.2', '【视觉】首页精准化v1.2', path.sep));
-
 
 module.exports = router;
