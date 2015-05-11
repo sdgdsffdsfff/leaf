c1e4ee511a2fbbc10d635d7b1615c9e5796a0e39
历风 <lb1064@qq.com>
2015-05-11 10:53:26 +0800

    搜索关键字简单高亮

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 var $projectForm = $('#projectForm');
 
 
-$projectForm.on('submit', function (e) {
+$projectForm.on('submit', function(e) {
     e.preventDefault();
     var data = new FormData(this);
     $.ajax({
@@ -10,20 +10,20 @@ $projectForm.on('submit', function (e) {
         contentType: false,
         processData: false,
         data: data
-    }).done(function (msg) {
+    }).done(function(msg) {
         drawProjectList();
         window.location.href = '#content';
         clearForm();
-    }).fail(function (msg) {
+    }).fail(function(msg) {
         alert(msg.responseText);
-        if(msg.responseText === '项目已存在!'){
+        if (msg.responseText === '项目已存在!') {
             var name = $('input[name=projectName]').val();
             $('#searchField').val(name).trigger('input').focus();
         }
     });
 });
 
-$('.formBox').on('click','.cancel',function(){
+$('.formBox').on('click', '.cancel', function() {
     clearForm();
 });
 
@@ -31,41 +31,49 @@ var $projectList = $('#projectList');
 var projectListCache = [];
 
 
-var fuse = new Fuse(projectListCache, { keys: ['name'] });
+var fuse = new Fuse(projectListCache, {
+    keys: ['name']
+});
 
 var str = '<li data-name="%name%">' +
-            '<a id="%name%"></a>' +
-            '<div class="desc">'+
-                '<h3>%name%</h3>' +
-                // '<span>%data% 更新</span>' +
-                '<p>%description%</p>' +
-            '</div>' + 
-            '<div class="links">' +
-                '<a href="/%name%#prd">PRD</a>' +
-                '<a href="/%name%#prototype" >交互</a>' +
-                '<a href="/%name%#visual">视觉</a>' +
-            '</div>' + 
-        '</li>';
+    '<a id="%name%"></a>' +
+    '<div class="desc">' +
+    '<h3>%showName%</h3>' +
+    // '<span>%data% 更新</span>' +
+    '<p>%description%</p>' +
+    '</div>' +
+    '<div class="links">' +
+    '<a href="/%name%#prd">PRD</a>' +
+    '<a href="/%name%#prototype" >交互</a>' +
+    '<a href="/%name%#visual">视觉</a>' +
+    '</div>' +
+    '</li>';
 
 function drawProjectList() {
     $.ajax({
         method: "POST",
         url: "/listProject"
-    }).done(function (msg) {
+    }).done(function(msg) {
         var html = '';
         if (msg.length > 0) {
             projectListCache = msg;
-            fuse = new Fuse(projectListCache, { keys: ['name'] });
-            msg.forEach(function (o, i) {
-                var t = str.replace(/(%(\w+)%)/g,function($1,$2,$3){
-                    return o[$3] ? o[$3] : '';
+            fuse = new Fuse(projectListCache, {
+                keys: ['name']
+            });
+            msg.forEach(function(o, i) {
+                var t = str.replace(/(%(\w+)%)/g, function($1, $2, $3) {
+                    if ($3 === 'showName') {
+                        return o['name'] ? o['name'] : '';
+                    } else {
+                        return o[$3] ? o[$3] : '';
+                    }
                 });
                 html += t;
             });
 
             $projectList.html('').html(html);
         }
-    }).fail(function (msg) {
+    }).fail(function(msg) {
         alert(msg);
     });
 }
@@ -75,17 +83,16 @@ drawProjectList();
 
 var headerHeight = $('header').height();
 
-$(window).on('scroll',function(e){
+$(window).on('scroll', function(e) {
     var _top = $(this).scrollTop();
 
     // 假如超过高度就悬浮
-    if(_top >= headerHeight){
-    }else{
+    if (_top >= headerHeight) {} else {
         // $('.searchPlaceholder').removeClass('fixed');
-        var scale = (2-_top/headerHeight)/2;
+        var scale = (2 - _top / headerHeight) / 2;
         $('header').css({
-            '-webkit-transform' : 'scale3d('+scale+','+scale+',1)',
-            'transform' : 'scale3d('+scale+','+scale+',1)'
+            '-webkit-transform': 'scale3d(' + scale + ',' + scale + ',1)',
+            'transform': 'scale3d(' + scale + ',' + scale + ',1)'
         })
     }
 })
@@ -93,16 +100,20 @@ $(window).on('scroll',function(e){
 
 var $searchField = $('#searchField');
 
-$searchField.on('input', function () {
+$searchField.on('input', function() {
 
-    var key  = $(this).val();
+    var key = $(this).val();
     var html = '无匹配结果!';
 
-    if(key.length === 0 ){
+    if (key.length === 0) {
         html = '';
-        projectListCache.forEach(function (o, i) {
-            var t = str.replace(/(%(\w+)%)/g,function($1,$2,$3){
-                return o[$3] ? o[$3] : '';
+        projectListCache.forEach(function(o, i) {
+            var t = str.replace(/(%(\w+)%)/g, function($1, $2, $3) {
+                if ($3 === 'showName') {
+                    return o['name'] ? o['name'] : '';
+                } else {
+                    return o[$3] ? o[$3] : '';
+                }
             });
             html += t;
         });
@@ -114,39 +125,41 @@ $searchField.on('input', function () {
 
     if (data.length > 0) {
         html = '';
-        data.forEach(function (o, i) {
-            var t = str.replace(/(%(\w+)%)/g,function($1,$2,$3){
-                return o[$3] ? o[$3] : '';
+        data.forEach(function(o, i) {
+            var sKey = '<div style="color: red;  display: inline;">' + key + '</div>';
+            var rStr = new RegExp(key, "g");
+
+            var t = str.replace(/(%(\w+)%)/g, function($1, $2, $3) {
+                if ($3 === 'showName') {
+                    return o['name'].replace(rStr, sKey) || '';
+                } else {
+                    return o[$3] ? o[$3] : '';
+                }
             });
             html += t;
         });
     }
     $projectList.html('').html(html);
 
-    window.location.href = '#content';
+    // 此处会阻断中文输入法
+    // window.location.href = '#content';
 
 });
 
-$('#projectList').on('click','li',function(){
+$('#projectList').on('click', 'li', function() {
     var name = $(this).data('name');
-    if(name){
-        window.location.href = '/'+name;
+    if (name) {
+        window.location.href = '/' + name;
     }
 })
 
 
-$('#addNew').on('click',function(){
+$('#addNew').on('click', function() {
     $('header .addBox').addClass('open');
     $('input[name=projectName]').focus();
 });
 
-function clearForm(){
+function clearForm() {
     $('header .addBox').removeClass('open');
     $projectForm[0].reset();
 }
-
-
-
-
-
-
